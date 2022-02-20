f2ed8144a0a755235b2d91e2e233b3019ba859bb
jaksehsekhja <jaksehsekhja@gmail.com>
2022-02-20 23:58:36 +0300

    email automation

--- email.py.py
+++ email.py.py
@@ -1,20 +1,25 @@
+from ast import For
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
 from_addr=input("enter Sender's Email ::  ")
-to_eml=input("enter reciever's email ::  ")
-to_addr=[to_eml]
+recv_num=int(input("how many recievers do you want   :: "))
+to_addr=[]
+for i in range(0,recv_num):
+    to_eml=input("enter reciever's "+str(i+1)+" email address::  ")
+    to_addr.append(to_eml)
+    
+
 msg=MIMEMultipart()
 msg['From']=from_addr
 msg['To']=" ,".join(to_addr)
-msg['subject']='just to check'
-
-body='hello world'
+subj=input("enter the subject of the email :: ")
+msg['subject']=subj    
+body=input("enter the body of the email ::   ")
 
 msg.attach(MIMEText(body,'plain'))
-
 email=from_addr
 password='5380jack'
 
@@ -24,5 +29,9 @@ mail.starttls()
 mail.login(email,password)
 text=msg.as_string()
 mail.sendmail(from_addr,to_addr,text)
+    
+
+
+
 print("email was sent successfully")
 mail.quit()
